81d8ecef70c28aaaaf8924aafe7b981d8e0dc0a1
Lawrence Karani <lawrencekarani@gmail.com>
2022-12-21 02:01:42 +0300

    added some recipe functionality

--- src/app/recipes/recipelist/recipelist.component.ts
+++ src/app/recipes/recipelist/recipelist.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipelist',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipelist.component.css']
 })
 export class RecipelistComponent {
+recipes:Recipe[] =[
+  new Recipe('A Test Recipe',
+  'This is simply a test',
+  'https://www.eatthis.com/wp-content/uploads/sites/4/2021/12/birria-tacos-1.jpg?quality=82&strip=1&w=1250'),
+  new Recipe('A Test Recipe',
+  'This is simply a test',
+  'https://www.eatthis.com/wp-content/uploads/sites/4/2021/12/birria-tacos-1.jpg?quality=82&strip=1&w=1250'),
+  new Recipe('A Test Recipe',
+  'This is simply a test',
+  'https://www.eatthis.com/wp-content/uploads/sites/4/2021/12/birria-tacos-1.jpg?quality=82&strip=1&w=1250')
+];
+
 
 }
